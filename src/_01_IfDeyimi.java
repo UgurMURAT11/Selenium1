79da633342f18ffa9e2eae8eaf80ce2fd134d1cb
90542 <ugur.murat.08.08@gmail.com>
2023-01-21 00:09:25 +0300

    for döngüsü örnek1 yapıldı

--- src/_01_IfDeyimi.java
+++ src/_01_IfDeyimi.java
@@ -2,5 +2,9 @@ public class _01_IfDeyimi {
     public static void main(String[] args) {
         System.out.println("Merhaba Dünya");
 
+        for (int i = 0; i <10 ; i++) {
+            System.out.println(i);
+
+        }
     }
 }
